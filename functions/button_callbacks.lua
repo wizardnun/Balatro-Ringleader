7860b195c127fe38694853099f7c5e1817ef73da
Vicky <vickythewebdev@gmail.com>
2025-12-31 14:03:04 -0600

    [Refactor] Move button callbacks into separate module

---
+++ functions/button_callbacks.lua
@@ -0,0 +1,8 @@
+-- Button callback for Collection Ringleaders button.
+function G.FUNCS.your_collection_ringleaders(e)
+  G.SETTINGS.paused = true
+
+	local t = create_UIBox_your_collection_ringleaders()
+
+  G.FUNCS.overlay_menu{definition = t}
+end
--- main.lua
+++ main.lua
@@ -1,12 +1,4 @@
 assert(SMODS.load_file("functions/misc_functions.lua"))()
 assert(SMODS.load_file("functions/ui_functions.lua"))()
-
--- Button callback for Collection Ringleaders button.
-function G.FUNCS.your_collection_ringleaders(e)
-  G.SETTINGS.paused = true
-
-	local t = create_UIBox_your_collection_ringleaders()
-
-  G.FUNCS.overlay_menu{definition = t}
-end
+assert(SMODS.load_file("functions/button_callbacks.lua"))()
 
