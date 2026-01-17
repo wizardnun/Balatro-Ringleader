b4d4d45296967d1db0c20bea0c43c5501e4c1212
Adeline See <adelinesee@protonmail.com>
2026-01-17 01:21:31 -0600

    [Chore] add utility function to get a ringleader's name

--- functions/misc_functions.lua
+++ functions/misc_functions.lua
@@ -9,3 +9,9 @@ function set_discover_tallies()
 		of = 10, -- TODO: Remove usage of static number.
 	}
 end
+
+-- Returns the name of a Ringleader 
+function get_ringleader_name(ringleader)
+	local loc = G.localization.descriptions.Ringleader[ringleader.key]
+	return loc and loc.name or ringleader.key
+end
