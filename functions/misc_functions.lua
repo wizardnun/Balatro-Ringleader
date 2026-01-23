8823f9c7470bc51503e97168434263c568041baf
Adeline See <adelinesee@protonmail.com>
2026-01-23 00:06:41 -0600

    [Feature] Update set_discover_tallies to correctly set tallies of discovered Ringleaders in the pool

--- functions/misc_functions.lua
+++ functions/misc_functions.lua
@@ -3,11 +3,15 @@ local setDiscoverTallies = set_discover_tallies
 -- Overwrites the global set_discover_tallies method in order to append a tally for Ringleaders.
 function set_discover_tallies()
 	setDiscoverTallies()
-
-	G.DISCOVER_TALLIES.ringleaders = {
-		tally = 0,
-		of = 10, -- TODO: Remove usage of static number.
-	}
+	G.DISCOVER_TALLIES.ringleaders = {tally = 0, of = 0}
+	G.DISCOVER_TALLIES.ringleaders.tally = 0
+	G.DISCOVER_TALLIES.ringleaders.of = 0
+	for _, v in pairs(G.P_CENTER_POOLS.Ringleader) do
+		G.DISCOVER_TALLIES.ringleaders.of = G.DISCOVER_TALLIES.ringleaders.of+1
+		if v.discovered then
+			G.DISCOVER_TALLIES.ringleaders.tally = G.DISCOVER_TALLIES.ringleaders.tally+1
+		end
+	end
 end
 
 -- Returns the name of a Ringleader 
